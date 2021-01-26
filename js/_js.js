6bbac7196887189278414e85e7ecee134e6660ed
Dasol Yoo <dasolyoo@Dasolui-MacBookAir.local>
2021-01-26 23:50:29 +0900

    script3

--- js/_js.js
+++ js/_js.js
@@ -1,3 +1,49 @@
+function _pipe() {
+  var fns = arguments; 
+  return function(arg) {
+    return _reduce(fns, function(arg, fn) { 
+      return fn(arg);
+    }, arg);
+  }
+}
+
+function _go(arg) { 
+  var fns = _rest(arguments); 
+  return _pipe.apply(null, fns)(arg);
+}
+
+var slice = Array.prototype.slice;
+function _rest(list, num) {
+  return slice.call(list, num || 1);
+}
+
+function _reduce(list, iter, memo) { 
+  if (arguments.length == 2) { 
+    memo = list[0];
+    list = _rest(list);
+  }
+  _each(list, function(val) {
+    memo = iter(memo, val);    
+  });
+  return memo;
+}
+
+function _curry(fn) {
+  return function(a, b) {
+    return arguments.length == 2 ? fn(a, b) : function(b) { return fn(a, b); };
+  }
+}
+
+function _curryr(fn) {
+  return function(a, b) { 
+    return arguments.length == 2 ? fn(a, b) : function(b) { return fn(b, a); };
+  }
+}
+
+var _get = _curryr(function(obj, key) {
+  return obj == null ? undefined : obj[key];
+});
+
 function _filter(list, predi) { 
   var new_list = [];
   _each(list, function(val) {
@@ -5,11 +51,6 @@ function _filter(list, predi) {
       new_list.push(val);
     }
   });
-  // for (var i = 0; i < list.length; i++) {
-  //   if (predi(list[i])) { 
-  //     new_list.push(list[i]);
-  //   }
-  // }
   return new_list; 
 }
 
@@ -18,15 +59,25 @@ function _map(list, mappper) {
   _each(list, function(val) {
     new_list.push(mappper(val));
   });
-  // for (var i = 0; i < list.length; i++) {
-  //   new_list.push(mappper(list[i]));
-  // }
   return new_list;
 }
 
-function _each(list, iter) {
-  for (var i = 0; i < list.length; i++) {
-    iter(list[i]);
+function _is_object(obj) {
+  return typeof obj == 'object' && !!obj;
+}
+function _keys(obj) {
+  return _is_object(obj) ? Object.keys(obj) : [];
+}
+
+var _length = _get('length');
+
+function _each(list, iter) { 
+  var keys = _keys(list);
+  for (var i = 0, len = keys.length; i < len; i++) {
+    iter(list[keys[i]], keys[i]);
   }
   return list;
-}
+}  
+
+var _map = _curryr(_map),
+  _filter = _curryr(_filter);
